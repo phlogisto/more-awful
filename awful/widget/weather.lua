1a3c1fff006f3cab15baff985ab2b98e6a12fc92
Predrag Mandic <predrag@phlogisto.com>
2014-01-28 23:56:17 +0100

    added weather widget (unfinished)

---
+++ awful/widget/weather.lua
@@ -0,0 +1,31 @@
+local setmetatable = setmetatable
+local io = io
+local capi = { widget = widget,
+               timer = timer }
+
+module("awful.widget.weather")
+
+
+local function update()
+  --TODO
+  return ""
+end
+
+function new(args)
+  local args      = args or {}
+  local timeout   = args.timeout or 300
+  local title     = args.title or ""
+  local subtitle  = args.subtitle or ""
+  args.type = "textbox"
+  local w = capi.widget(args)
+  local timer = capi.timer { timeout = timeout }
+  w.text = title..update()..subtitle
+  timer:add_signal("timeout", function() w.text = title..update()..subtitle end)
+  timer:start()
+  return w
+end
+
+
+setmetatable(_M, { __call = function(_, ...) return new(...) end })
+
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=2:softtabstop=2:textwidth=80
